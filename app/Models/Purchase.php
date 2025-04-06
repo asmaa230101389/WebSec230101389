ad491c0f2a2155d6fccf489c5e53f38761f10d7f
Asmaa-25112003 <asmaa230101389@sut.edu.eg>
2025-04-06 22:14:48 +0200

    Midterm Assignment

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -8,13 +8,13 @@ class Purchase extends Model
 {
     protected $fillable = ['user_id', 'product_id', 'quantity', 'total_price'];
 
-public function user()
-{
-    return $this->belongsTo(User::class);
-}
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
-public function product()
-{
-    return $this->belongsTo(Product::class);
-}
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
